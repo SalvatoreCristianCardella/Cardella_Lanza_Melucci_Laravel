08baa94470e989f52504e60a5a354c31ac2d0f12
Lanza Benedetto <bennylanza@gmail.com>
2023-06-21 15:22:55 +0200

    sistemato problema card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,7 @@
 
 
 <div class="card" style="width: 18rem;">
-    <img src="{{$cardImg}}" class="card-img-top" alt="...">
+    <img src="{{$cardImg}}" class="card-img-top" style="height:300px" alt="{{$cardImg}}">
     <div class="card-body">
       <h5 class="card-title">{{$cardName}}</h5>
       <h5 class="card-title">{{$cardSurname}}</h5>
--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -8,13 +8,13 @@
             @foreach ($students as $student)
                 <div class="col-12 col-md-3">
                   <x-card
-                  cardImg= "{{$student ['Img']}}"
-                  cardName= "{{$student ['Name']}}"
-                  cardSurname= "{{$student ['Surname']}}"
-                  cardAge= "{{$student ['Age']}}"
-                  cardLanguage= "{{$student ['Language']}}"
-                  cardCity= "{{$student ['City']}}"
-                  cardId= "{{$student ['id']}}"
+                  cardImg="{{$student ['Img']}}"
+                  cardName="{{$student ['Name']}}"
+                  cardSurname="{{$student ['Surname']}}"
+                  cardAge="{{$student ['Age']}}"
+                  cardLanguage="{{$student ['Language']}}"
+                  cardCity="{{$student ['City']}}"
+                  cardId="{{$student ['id']}}"
                   />
                 </div>
             @endforeach
